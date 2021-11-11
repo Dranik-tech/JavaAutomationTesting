32beb8f98b2fbe8f2334039773529977be7bd979
Andrei Korol <andrei.korol1987@gmail.com>
2021-11-11 22:15:14 +0300

    added comments

--- test/src/main/java/MainClass.java
+++ test/src/main/java/MainClass.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class MainClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver","D:\\Java_projects\\JavaAutomationTesting\\test\\drivers\\geckodriver.exe");
-
+//test comment
         WebDriver driver = new FirefoxDriver();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
