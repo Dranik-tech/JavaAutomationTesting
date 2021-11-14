829a5fffd3e7c50ff0bb80d5cd650c8a14ebb9fa
Andrei Korol <andrei.korol1987@gmail.com>
2021-11-14 19:20:09 +0300

    finished with 2) Отобразить в окне консоли аргументы командной строки в обратном порядке.

--- test/src/main/java/MainClass.java
+++ test/src/main/java/MainClass.java
@@ -1,21 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.junit.Assert;
-import java.time.Duration;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 public class MainClass {
     public static void main(String[] args) {
-        Scanner inputData = new Scanner(System.in);
 
-        String greatingText = inputData.nextLine();
-
-        System.out.println("Hello, " + greatingText);
+        for(int j = args.length - 1; j >= 0; j--){
+            System.out.println(args[j]);
+        }
     }
 }
